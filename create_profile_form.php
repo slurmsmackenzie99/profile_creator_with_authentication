007e5e16b084041d8291c561a59658333be40bf3
slurms_mackenzie <50649298+slurmsmackenzie99@users.noreply.github.com>
2021-06-30 16:46:06 +0200

    Initial commit

---
+++ create_profile_form.php
@@ -0,0 +1,76 @@
+<?php
+$name = "";
+$character = "";
+$email = "";
+$birth_year = 1969;
+$validation_error = "";
+$existing_users = ["admin", "guest"];
+$existing_characters = ["wizard", "mage", "orc"];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = $_POST["name"];
+  $raw_name = trim(htmlspecialchars($name));
+  if(in_array($raw_name, $existing_users)){
+    $validation_error .= "This name is taken. <br>";
+  } else {
+    $name = $raw_name;
+  }
+
+  $raw_character = $_POST["character"];
+  if(in_array($raw_character, $existing_characters)){
+    $character = $raw_character;
+  } else {
+    $validation_error .= "You must pick a wizard, mage, or orc. <br>";
+  }
+  $raw_email = $_POST["email"];
+  if(filter_var($raw_email, FILTER_VALIDATE_EMAIL)){
+    $email = $raw_email;
+  } else {
+    $validation_error .= "Invalid email. <br>";
+  }
+
+  $raw_birth_year = $_POST["birth_year"];
+  $options = ["minimum_value" => 1930, "maximum_value" => date("Y")];
+  if(filter_var($raw_birth_year)){
+
+  }
+}
+?>
+<h1>Create your profile</h1>
+<form method="post" action="">
+<p>
+Select a name: <input type="text" name="name" value="<?php echo $name;?>" >
+</p>
+<p>
+Select a character:
+  <input type="radio" name="character" value="wizard" <?php echo ($character=='wizard')?'checked':'' ?>> Wizard
+  <input type="radio" name="character" value="mage" <?php echo ($character=='mage')?'checked':'' ?>> Mage
+  <input type="radio" name="character" value="orc" <?php echo ($character=='orc')?'checked':'' ?>> Orc
+</p>
+<p>
+Enter an email:
+<input type="text" name="email" value="<?php echo $email;?>" >
+</p>
+<p>
+Enter your birth year:
+<input type="text" name="birth_year" value="<?php echo $birth_year;?>">
+</p>
+<p>
+  <span style="color:red;"><?= $validation_error;?></span>
+</p>
+<input type="submit" value="Submit">
+</form>
+
+<h2>Preview:</h2>
+<p>
+  Name: <?=$name;?>
+</p>
+<p>
+  Character Type: <?=$character;?>
+</p>
+<p>
+  Email: <?=$email;?>
+</p>
+<p>
+  Age: <?=date("Y")-$birth_year;?>
+</p>
